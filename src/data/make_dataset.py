b364d4da64254f21e3eb7986e1e7b88460cf85a4
KHiMAeRA05 <khaios@mail.ru>
2023-11-05 16:33:42 +0300

    .

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,4 +6,4 @@ mask = df['trn_tox'] > df['ref_tox']
 # Swap the values of reference and translation, and ref_tox and trn_tox where the condition is True
 df.loc[mask, ['reference', 'translation', 'ref_tox', 'trn_tox']] = df.loc[mask, ['translation', 'reference', 'trn_tox', 'ref_tox']].values
 
-df.to_csv('/data/interim/dataset.csv', index=False)  
+df.to_csv('../../data/interim/dataset.csv', index=False)  
